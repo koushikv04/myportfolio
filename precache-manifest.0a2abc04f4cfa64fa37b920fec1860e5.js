05c20ae1fb1122760df60c5334028e800ffdb094
koushik V <koushikv@koushiks-MacBook-Pro-2.local>
2020-01-11 20:15:23 +0000

    Updates

---
+++ precache-manifest.0a2abc04f4cfa64fa37b920fec1860e5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12c6285420447fe90968f8fb824ad169",
+    "url": "/myportfolio/index.html"
+  },
+  {
+    "revision": "fff0019059a7fba82acc",
+    "url": "/myportfolio/static/css/main.b529fc54.chunk.css"
+  },
+  {
+    "revision": "6ee72592e5050ffa59a3",
+    "url": "/myportfolio/static/js/2.4e55c1f4.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/myportfolio/static/js/2.4e55c1f4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "fff0019059a7fba82acc",
+    "url": "/myportfolio/static/js/main.81baaee9.chunk.js"
+  },
+  {
+    "revision": "d92861c97cac94112c81",
+    "url": "/myportfolio/static/js/runtime-main.137c290a.js"
+  },
+  {
+    "revision": "7ddee684aca6987e9af4553647dcc991",
+    "url": "/myportfolio/static/media/portfoliobg.7ddee684.png"
+  }
+]);
